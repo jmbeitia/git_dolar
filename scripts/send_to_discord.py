871de6ee4531513a2b2383dc5cbec76da87ed189
jmbeitia <joaquin.manuel.beitia@gmail.com>
2024-02-09 00:22:00 -0300

    cambio data de lugar

--- scripts/send_to_discord.py
+++ scripts/send_to_discord.py
@@ -10,13 +10,33 @@ def send_message(webhook_url, message):
     else:
         print(f"Failed to send message. Status code: {response.status_code}")
 
+
+def make_message(data):
+            
+        table = "```Precios del dolar\n"
+        table += "| Casa                     | Compra    | Venta     | Fecha de Actualización       |\n|--------------------------|-----------|-----------|------------------------------|\n"
+
+        for entry in data:
+            casa = entry.get("nombre")
+            compra = entry.get("compra")
+            venta = entry.get("venta")
+            fecha_actualizacion = entry.get("fechaActualizacion")
+
+            # Add each row to the table
+            table += f"| {casa:<24} | {compra:<9} | {venta:<9} | {fecha_actualizacion:<28} |\n"
+        
+        table += "```"
+        return table
+
 if __name__ == "__main__":
     # Get the scraped data from the environment
-    message = os.environ.get("DISCORD_MESSAGE")
+    data = os.environ.get("DISCORD_MESSAGE")
     
-    if not message:
+    if not data:
         print("Error: No scraped data found.")
         sys.exit(1)
+
+    message = make_message(data)
 
     # Get the Discord webhook URL from the environment variable
     webhook_url = os.environ.get("DISCORD_WEBHOOK_URL")
